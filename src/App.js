2e4fb1c06f7eeda773a6be6e48a01447b8ea3e6a
srbony <srabonycse@gmail.com>
2022-11-08 10:27:44 +0600

    added footer

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import { RouterProvider } from 'react-router-dom';
+import './App.css';
+import router from './Router/Routes/Routes';
+
+function App() {
+
+
+
+
+
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+
+    </div>
+  );
+}
+
+export default App;
